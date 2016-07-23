b34a29e62fa103c9ac0bbc2833b51e0d82045b04
kwonsm <kwonsm@172.20.10.3>
2016-07-23 23:21:27 +0900

    send() 개발 완료 및 기본 상태는 성공으로 수정

--- HelloMessage_APIServer/src/team/hellobro/gw/api/transform/Response.java
+++ HelloMessage_APIServer/src/team/hellobro/gw/api/transform/Response.java
@@ -5,6 +5,8 @@ import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpUtil;
@@ -22,6 +24,11 @@ public class Response
 {
 	private HttpResponseStatus status;
 	private String resultCode;
+	
+	public Response()
+	{
+		this.success();
+	}
 	
 	public void success()
 	{
@@ -49,7 +56,6 @@ public class Response
 	{
 		StringBuilder str = new StringBuilder();
 		str.append("result code:").append(this.resultCode);
-		str.append("\n");
 		
 		return str.toString();
 	}
@@ -60,12 +66,18 @@ public class Response
 		ByteBuf content = Unpooled.copiedBuffer(responseXml, CharsetUtil.UTF_8);
 		
 		FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, this.status, content);
-		response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8");
+		HttpHeaders responseHeader = response.headers();
+		responseHeader.set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
 		
 		FullHttpRequest httpRequest = RequestContext.get(RequestContextKey.HTTP_REQUEST);
 		if(HttpUtil.isKeepAlive(httpRequest))
 		{
-			
+			// Add 'Content-Length' header only for a keep-alive connection.
+			responseHeader.setInt(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+			// Add keep alive header as per:
+			responseHeader.set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
 		}
+		
+		RequestContext.writeResponse(response);
 	}
 }
